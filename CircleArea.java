6c2ced2ff2109b54449e46f491e360091abf5dfe
PushpalathaMookaiah <105479776+PushpalathaMookaiah@users.noreply.github.com>
2022-07-14 21:09:16 +0530

    Add files via upload

---
+++ CircleArea.java
@@ -0,0 +1,14 @@
+public class CircleArea
+{
+    public static void main(String[] args)
+    {
+    float val=3.14f;
+    int radius=2;
+    float area;
+
+    area=val*radius*radius;
+
+    System.out.println("Circle Area="+area);
+
+    }
+}
